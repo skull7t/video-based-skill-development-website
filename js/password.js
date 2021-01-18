6cc87c692bd155e32b720dd59ba32cd535b13c9d
Rohit Patil <43942305+skull7t@users.noreply.github.com>
2021-01-19 01:19:18 +0530

    Add files via upload

---
+++ js/password.js
@@ -0,0 +1,11 @@
+function myfun(){
+    var a = document.getElementBy("password").value;
+    
+    var b = document.getElementBy("password").value
+    
+    
+    if(a!==b){
+        document.getElementById("\")
+    }
+    
+}
